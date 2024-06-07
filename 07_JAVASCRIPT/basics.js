33028a2674d9ea86ab75440e2e56be9ed2a69bc8
akhil-achu <akhilmg501@gmail.com>
2024-06-07 16:19:02 +0530

    update

---
+++ 07_JAVASCRIPT/basics.js
@@ -0,0 +1,26 @@
+// javascript basic
+
+// 1) variable - name memory location 
+        //  variable declaraton
+        //   a=10;
+        //   var a=10;
+        //   let a=10;
+        //   const a=10;
+// 2) comment
+          // 1) single line comment -
+          
+        /*..........................
+        .......................................................
+        ..........................*/
+        // 3) display statement using console.log
+             //print message
+             console.log("Hello World")  
+
+             console.log(7736864505);
+
+             let message ="Goodbye Wolrd"
+             console.log(message);
+             
+             
+
+        
